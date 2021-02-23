023410f4b05a9f02f0ebf26129e80a3d64711c14
taha2fly <taha.elrobrini@gmail.com>
2021-02-23 11:37:50 +0100

    Solution de l'exercice 2 POO : classe Point

---
+++ exo2.js
@@ -0,0 +1,36 @@
+
+class Point{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+	}
+
+	getX(){
+		return this.x;
+	}
+
+	getY(){
+		return this.y;
+	}
+
+	setX(x){
+		this.x = x;
+	}
+
+	setY(y){
+
+		this.y = y;
+	}
+
+	toString(){
+		return `Point (${this.x}, ${this.y})`;
+	}
+}
+
+
+p1 = new Point(10, 50);
+console.log(p1.toString());
+p1.setX(20);
+console.log(p1.toString());
+p1.setY(20);
+console.log(p1.toString());
