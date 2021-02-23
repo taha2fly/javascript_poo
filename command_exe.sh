0b5517c9b321e57729a6982a935e7feff6b61ecf
taha2fly <taha.elrobrini@gmail.com>
2021-02-23 12:00:39 +0100

    commande modifiée pour le nouvel exo, exo3 Classe Cercle

--- command_exe.sh
+++ command_exe.sh
@@ -1,4 +1,4 @@
 # /bin/bash
 
-sudo docker run --rm -ti -v /home/taha/Documents/Exercices_javascript/javascript_poo:/app -w /app node node exo2.js
+sudo docker run --rm -ti -v /home/taha/Documents/Exercices_javascript/javascript_poo:/app -w /app node node exo3.js
 
